8029ccf96a2c951b05dc77a2002b17c7d4135c44
Aman Singh <amanpratap1437@gmail.com>
2025-04-17 11:38:47 +0530

    portfolio websites:

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:myemail@email.com" target="_blank">amanpratap1437@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/aman-singh-a1a914203/" target="_blank">linkedin.com/amansingh</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/amanDev1437" target="_blank">github.com/amanDev1437</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  server: {
+    host: '0.0.0.0', // Allow access from network
+    port: 5173,       // Or any other port you prefer
+  }     // Or any other port you prefer
+}
+);
+
+
